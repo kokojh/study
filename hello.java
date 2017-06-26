e31eec489d8a330a9f139bd029d82bd2660233e5
ko <kjhzzang4692@naver.com>
2017-06-26 12:43:22 +0900

    added output "Tell your Number"

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Hello world2");
+		System.out.println("Tell your number");
 	}
 }
