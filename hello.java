c782706b988e6e1e46e66206b58c3c701f49b0b1
ko <kjhzzang4692@naver.com>
2017-06-26 12:52:41 +0900

    add Tell his world

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ public class Hello{
 		System.out.println("Hello World");
 		System.out.println("Hello world2");
 		System.out.println("Tell your number");
+		System.out.println("Tell my name")
 	}
 }
