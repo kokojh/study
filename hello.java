d70465dadabe9bc2311b51141d43c8342d3f144a
kokojh <kjhzzang4692@naver.com>
2017-06-27 09:25:32 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,5 +5,6 @@ public class Hello{
 		System.out.println("Hello world2");
 		System.out.println("Tell your number");
 		System.out.println("Tell my name")
+		System.out.println("Tell my world");
 	}
 }
